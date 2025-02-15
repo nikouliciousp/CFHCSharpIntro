bb6698f1dbcaac29ab961c646aef8866d0c8f71d
perisnik <nikouliciousp@gmail.com>
2025-02-16 00:11:10 +0200

    v.1.1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace HelloWorld
+﻿using System;
+using static System.Math;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Coding is fun!");
         }
     }
 }
