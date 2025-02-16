c906ebc7c0c4365a659c18ff92a29a82cf9d70d2
perisnik <nikouliciousp@gmail.com>
2025-02-16 15:09:11 +0200

    Add NullablesApp project and update HelloWorld
    - Updated solution file to include new project `NullablesApp` targeting .NET 8.0 with nullable reference types.
    - Enhanced `HelloWorld`'s `Program.cs` with XML documentation and added a console output for "Coding is fun!".
    - Created `Program.cs` for `NullablesApp` demonstrating nullable types and their usage.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,11 +3,22 @@ using static System.Math;
 
 namespace HelloWorld
 {
+    /// <summary>
+    /// Main class
+    /// This is the entry point of the HelloWorld application.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args">Array of command-line arguments.</param>
         static void Main(string[] args)
         {
+            // Print "Hello World!" to the console
             Console.WriteLine("Hello World!");
+
+            // Print "Coding is fun!" to the console
             Console.WriteLine("Coding is fun!");
         }
     }
---
+++ NullablesApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace NullablesApp
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Declare a nullable integer
+            int num = 5;
+
+            // Declare a nullable integer
+            int? i = null;
+
+            // Display the value of nullable integer
+            Console.WriteLine(num);
+
+            // Display the value of nullable integer
+            Console.WriteLine("i: {0}", (i == null) ? "null" : "not null");
+
+            // Display the value of nullable integer
+            Console.WriteLine(i.GetValueOrDefault(10));
+        }
+    }
+}
