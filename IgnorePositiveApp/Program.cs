dbf60e3ad42ec73b7ef4f0674643bb55ab99bc22
perisnik <nikouliciousp@gmail.com>
2025-02-16 21:36:25 +0200

    Add IgnorePositiveApp project with number filtering logic
    - Introduced a new project "IgnorePositiveApp" in the solution.
    - Created `IgnorePositiveApp.csproj` targeting .NET 8.0 with necessary settings.
    - Implemented a `Program` class that processes user input to ignore positive numbers and print negative ones.

---
+++ IgnorePositiveApp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace IgnorePositiveApp
+{
+    /// <summary>
+    /// This class ignores positive numbers and prints negative numbers.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The entry point of the application.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            Console.WriteLine("Please insert different numbers. Insert 0 to finish.");
+
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine("Invalid number.");
+                Environment.Exit(1);
+            }
+
+            while (number != 0)
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine("Positive numbers are not allowed.");
+                }
+                else
+                {
+                    Console.WriteLine($"Number {++count}: {number}");
+                }
+                if (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Invalid number.");
+                    Environment.Exit(1);
+                }
+            }
+        }
+    }
+}
