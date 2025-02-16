2198b28ca5361decbe4ed21a1d3697f33cb51202
perisnik <nikouliciousp@gmail.com>
2025-02-16 23:12:11 +0200

    Add FiboIterApp project with Fibonacci iterator implementation
    - Introduced a new project "FiboIterApp" in the solution.
    - Updated solution configurations for Debug and Release builds.
    - Created project file `FiboIterApp.csproj` with necessary properties.
    - Added `Program.cs` implementing a Fibonacci series generator using an iterator.

---
+++ FiboIterApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace FiboIterApp
+{
+    /// <summary>
+    /// Fibonacci series using iterator
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci series using iterator");
+            Console.WriteLine("Enter the number of terms in the series");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Display the series
+            foreach (int i in Fibonacci(n))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        /// <summary>
+        /// Fibonacci series using iterator
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static IEnumerable<int> Fibonacci(int n)
+        {
+            int a = 0, b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                yield return a;
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+        }
+    }
+}
