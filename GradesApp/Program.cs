e0a80297df6d17a55996cd3942494c4a014f8054
perisnik <nikouliciousp@gmail.com>
2025-02-16 20:39:41 +0200

    Add GradesApp project with grade calculation feature
    This commit introduces a new project named "GradesApp" to the solution.
    It includes the project configuration in `CFHCSharpIntro.sln` and a new
    `GradesApp.csproj` file targeting .NET 8.0. The `Program.cs` file
    contains logic for prompting users to enter grades, calculating the
    average, and validating input.

---
+++ GradesApp/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace GradesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradesSum = 0;
+            int gradesCount = 0;
+            string? input;
+
+            Console.WriteLine("Enter your grades, type 'done' when you're finished");
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == null)
+                {
+                    Console.WriteLine("Input cannot be null");
+                    continue;
+                }
+
+                if (input.ToLower() == "done")
+                {
+                    break;
+                }
+
+                if (int.TryParse(input, out int grade))
+                {
+                    gradesSum += grade;
+                    gradesCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid grade");
+                }
+            }
+
+            if (gradesCount == 0)
+            {
+                Console.WriteLine("You didn't enter any grades");
+            }
+            else
+            {
+                int gradesAverage = gradesSum / gradesCount;
+                Console.WriteLine($"Your average grade is {gradesAverage}");
+            }
+        }
+    }
+}
