970c0c0703fdb7d80139a8725cf41cb5f34bbc34
perisnik <nikouliciousp@gmail.com>
2025-02-22 19:10:46 +0200

    Add SearchApp project with linear search implementation
    - Introduced a new project "SearchApp" in the solution.
    - Created `Program.cs` with a linear search algorithm.
    - Added `SearchApp.csproj` with .NET 8.0 settings and configurations.

---
+++ SearchApp/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SearchApp
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int key = 5;
+            int position = Search(arr, key);
+            if (position != -1)
+            {
+                Console.WriteLine("Element found at index: " + position);
+            }
+            else
+            {
+                Console.WriteLine("Element not found");
+            }
+        }
+
+        /// <summary>
+        /// Search for a key in an array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static int Search(int[] arr, int key)
+        {
+            int position = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    position = i;
+                    break;
+                }
+            }
+            return position;
+        }
+    }
+}
