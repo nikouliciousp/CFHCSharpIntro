0e6c66f5673637cc85bb3fd204e949649373708d
perisnik <nikouliciousp@gmail.com>
2025-02-22 18:36:43 +0200

    Add FactorialApp project and implementation
    - Introduced a new project "FactorialApp" in the solution.
    - Updated solution configuration to include build settings for "FactorialApp".
    - Modified `Fibonacci` method in `Program.cs` to clarify its functionality.
    - Created `FactorialApp.csproj` with necessary .NET SDK properties.
    - Added `Program` class with a `Main` method to calculate and display factorials.

---
+++ FactorialApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace FactorialApp
+{
+    /// <summary>
+    /// FactorialApp Program class.
+    /// This class is the entry point of the application.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int factorial = 1;
+
+            System.Console.Write("Enter a number: ");
+
+            if (int.TryParse(System.Console.ReadLine(), out int number))
+            {
+                factorial = CalculateFactorial(number);
+                System.Console.WriteLine($"Factorial of {number} is {factorial}");
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        /// <summary>
+        /// Calculate factorial of a number.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int CalculateFactorial(int number)
+        {
+            if (number < 0)
+            {
+                return 0;
+            }
+           
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * CalculateFactorial(number - 1);
+        }
+    }
+}
--- FiboIterApp/Program.cs
+++ FiboIterApp/Program.cs
@@ -33,6 +33,7 @@
             int a = 0, b = 1;
             for (int i = 0; i < n; i++)
             {
+                // Return the current element
                 yield return a;
                 int temp = a;
                 a = b;
