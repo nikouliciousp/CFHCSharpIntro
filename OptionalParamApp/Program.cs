37b93f86ae791a06f8c47754887c84b8c52ffdf8
perisnik <nikouliciousp@gmail.com>
2025-03-01 01:55:38 +0200

    Add OptionalParamApp project with date utility
    - Introduced a new project "OptionalParamApp" in the solution.
    - Created `OptionalParamApp.csproj` targeting .NET 8.0 with necessary configurations.
    - Added `Program.cs` containing a `Main` method that demonstrates the use of a `GetDate` method with optional parameters.
    - Implemented `GetDate` method to allow flexible date creation using default parameter values.

---
+++ OptionalParamApp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace OptionalParamApp
+{
+    /// <summary>
+    /// Main program class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            DateTime date = GetDate(2020, 1, 1, 12, 0, 0, 0);
+            Console.WriteLine(date);
+
+            date = GetDate(2020, 1, 1);
+            Console.WriteLine(date);
+
+            date = GetDate(day:1, hour:3);
+            Console.WriteLine(date);
+        }
+
+        /// <summary>
+        /// Get a date with optional parameters
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="day"></param>
+        /// <param name="hour"></param>
+        /// <param name="min"></param>
+        /// <param name="sec"></param>
+        /// <param name="milli"></param>
+        /// <returns></returns>
+        public static DateTime GetDate(int year = 1970, int month = 1, int day = 1, int hour = 0, int min = 0, int sec = 0, int milli = 0)
+        {
+            return new DateTime(year, month, day, hour, min, sec, milli, DateTimeKind.Utc);
+        }
+    }
+}
