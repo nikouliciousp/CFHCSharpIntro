ccd35281abb87957e3dfc11573fe432d8b8ea86d
perisnik <nikouliciousp@gmail.com>
2025-03-02 00:42:48 +0200

    Add AccountApp project with basic functionality
    - Created a new project "AccountApp" in the solution.
    - Added project file `AccountApp.csproj` with SDK and settings.
    - Implemented a `Program.cs` with a simple "Hello, World!" output.
    - Introduced `InsuffisientAmountException` and `NegativeAmountException` for error handling.
    - Updated `Account.cs` to manage deposits and withdrawals with proper exception handling.

---
+++ AccountApp/Model/Account.cs
@@ -0,0 +1,68 @@
+﻿using AccountApp.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountApp.Model
+{
+    internal class Account
+    {
+        public long Id { get; set; }
+        public string? Iban { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? SSN { get; set; }
+        public decimal Balance { get; set; }
+
+        /// <summary>
+        /// Withdraws the specified amount from the account
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Deposit(decimal amount)
+        {
+            try
+            {
+                if (amount > 0)
+                {
+                    Balance += amount;
+                }
+                else
+                {
+                    throw new NegativeAmountException("Amount must be greater than 0");
+                }
+            }
+            catch (NegativeAmountException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Withdraws the specified amount from the account
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Withdraw(decimal amount)
+        {
+            try
+            {
+                if (amount > 0)
+                {
+                    Balance -= amount;
+                }
+                else
+                {
+                    throw new InsuffisientAmountException("Amount must be greater than 0");
+                }
+            }
+            catch (InsuffisientAmountException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+        }
+    }
+}
