e3702358f71f2a2359e16d42deea2944aec632ba
perisnik <nikouliciousp@gmail.com>
2025-03-02 23:52:32 +0200

    Add OperatorOverloadingApp project with Fraction class
    Updated solution to include OperatorOverloadingApp project.
    Created Fraction class with operator overloads for arithmetic.
    Added project file for .NET 8.0 and entry point in Program.cs.

---
+++ OperatorOverloadingApp/Fraction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingApp
+{
+    /// <summary>
+    /// Represents a fraction with a numerator and a denominator.
+    /// </summary>
+    internal class Fraction
+    {
+        public int Num { get; init; }
+        public int Den { get; init; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Fraction"/> class.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="den"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public Fraction(int num, int den)
+        {
+            if (den == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero");
+            }
+            Num = num;
+            Den = den;
+        }
+
+        /// <summary>
+        /// Operator overloads for the Fraction class.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static Fraction operator +(Fraction a) => a;
+        public static Fraction operator -(Fraction a) => new Fraction(-a.Num, a.Den);
+        public static Fraction operator +(Fraction a, Fraction b) => new Fraction(a.Num * b.Den + b.Num * a.Den, a.Den * b.Den);
+        public static Fraction operator -(Fraction a, Fraction b) => a + (-b);
+        public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a.Num * b.Num, a.Den * b.Den);
+        public static Fraction operator /(Fraction a, Fraction b) => new Fraction(a.Num * b.Den, a.Den * b.Num);
+
+        /// <summary>
+        /// Returns a string representation of the fraction.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => $"{Num}/{Den}";
+    }
+}
---
+++ OperatorOverloadingApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace OperatorOverloadingApp
+{
+    /// <summary>
+    /// Represents a fraction with a numerator and a denominator.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Create two objects of the Fraction class
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 3);
+
+            // Add the two fractions
+            Fraction f3 = f1 + f2;
+            Console.WriteLine($"{f1} + {f2} = {f3}");
+
+            // Subtract the two fractions
+            Fraction f4 = f1 - f2;
+            Console.WriteLine($"{f1} - {f2} = {f4}");
+
+            // Multiply the two fractions
+            Fraction f5 = f1 * f2;
+            Console.WriteLine($"{f1} * {f2} = {f5}");
+
+            // Divide the two fractions
+            Fraction f6 = f1 / f2;
+            Console.WriteLine($"{f1} / {f2} = {f6}");
+
+            // Negate a fraction
+            Fraction f7 = -f1;
+            Console.WriteLine($"-{f1} = {f7}");
+        }
+    }
+}
