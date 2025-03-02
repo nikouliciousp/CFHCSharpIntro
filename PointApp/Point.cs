33b392fe0cb8bfc34ea5dd7beb784ee04bebcb9f
perisnik <nikouliciousp@gmail.com>
2025-03-02 20:59:30 +0200

    Add PointApp project with Point and Point2D classes
    - Introduced a new project "PointApp" in the solution.
    - Updated solution configuration for the new project.
    - Added `Point` and `Point2D` classes for coordinate management.
    - Created `PointApp.csproj` targeting .NET 8.0.
    - Added an empty `Program` class as the application entry point.

---
+++ PointApp/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointApp
+{
+    internal class Point
+    {
+        public int X { get; set; }
+
+        /// <summary>
+        /// Get a new point
+        /// </summary>
+        /// <returns></returns>
+        public static Point GetPoint()
+        {
+            return new Point();
+        }
+
+        /// <summary>
+        /// Move the point by 10 units
+        /// </summary>
+        public virtual void Move10()
+        {
+            X += 10;
+        }
+
+        /// <summary>
+        /// Print the point
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => $"X: {X}";
+    }
+}
---
+++ PointApp/Point2D.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointApp
+{
+    internal class Point2D: Point
+    {
+        public int Y { get; set; }
+
+        //public Point2D() : base()
+        //{
+        //    Y = 0;
+        //}
+
+        /// <summary>
+        /// Static method to get a new Point2D
+        /// </summary>
+        /// <returns></returns>
+        public static Point2D GetPoint2D()
+        {
+            return new Point2D();
+        }
+
+        /// <summary>
+        /// Move the point by 10 units
+        /// </summary>
+        public override void Move10()
+        {
+            base.Move10();
+            Y += 10;
+        }
+
+        /// <summary>
+        /// Print the point
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => $"{base.ToString()}, Y: {Y}";
+    }
+}
