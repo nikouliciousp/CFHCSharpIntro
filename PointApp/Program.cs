b5e433ea7c71ae6834ebe80fbb5d5ea715ed309d
perisnik <nikouliciousp@gmail.com>
2025-03-02 21:02:21 +0200

    Enhance Main method to handle Point and Point2D
    Updated the `Main` method in the `Program` class to create and manipulate `Point` and `Point2D` objects. Added functionality to call their `Move10()` methods and print their string representations. Removed the previous line of code to implement these changes.

--- PointApp/Program.cs
+++ PointApp/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            Point p = Point.GetPoint();
+            p.Move10();
+            Console.WriteLine(p.ToString());
+
+            Console.WriteLine();
+            Point2D p2 = Point2D.GetPoint2D();
+            p2.Move10();
+            Console.WriteLine(p2.ToString());
+
         }
     }
 }
