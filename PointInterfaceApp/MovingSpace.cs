9e036bcd8532942d7070c6de289564250d1bbce4
perisnik <nikouliciousp@gmail.com>
2025-03-03 00:34:13 +0200

    Add MovingSpace class for object movement handling
    Introduces the `MovingSpace` class in the `PointInterfaceApp` namespace. This class manages the movement of objects implementing the `IMovable` interface, utilizing dependency injection for instantiation. It features a `MakeMove` method that invokes the `Move5` method on the `Movable` object to facilitate movement functionality.

---
+++ PointInterfaceApp/MovingSpace.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointInterfaceApp
+{
+    /// <summary>
+    /// This class is responsible for moving the object
+    /// </summary>
+    class MovingSpace
+    {
+        // Interface object
+        private IMovable Movable;
+
+        // Dependency Injection (DI) - Invertion of Control (IoC)
+        public MovingSpace(IMovable movable)
+        {
+            this.Movable = movable;
+        }
+
+        /// <summary>
+        /// Make a move
+        /// </summary>
+        public void MakeMove()
+        {
+            Movable.Move5();
+        }
+    }
+}
