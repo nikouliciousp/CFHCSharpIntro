bc19077e00e463a4ac0e933cb1ffb6edebef9bfc
perisnik <nikouliciousp@gmail.com>
2025-03-16 19:35:26 +0200

    Add ReadWriteApp project with file copy functionality
    - Introduced a new project "ReadWriteApp" in the solution.
    - Updated solution configuration for Debug and Release builds.
    - Created `Program.cs` for a console app that copies file contents.
    - Added `ReadWriteApp.csproj` with .NET 8.0 settings and properties.

---
+++ ReadWriteApp/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ReadWriteApp
+{
+    /// <summary>
+    /// Main entry point of the application
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry point of the application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create a FileStream object for reading and writing
+                using FileStream fws = new(@"C:\temp\test-out.txt", FileMode.Append);
+                using FileStream frs = new(@"C:\temp\test-read.txt", FileMode.Open);
+
+                // Create a StreamReader and StreamWriter object
+                using StreamReader sr = new(frs);
+                using StreamWriter sw = new(fws);
+
+                // Read the file
+                string? line;
+
+                // Read the file
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(line);
+                }
+
+                Console.WriteLine("File copied successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
