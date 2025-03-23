da447a48d81a33ac2d90e6d736a940491523e08c
perisnik <nikouliciousp@gmail.com>
2025-03-23 22:20:49 +0200

    Add comments to Program.cs for clarity and context
    Enhanced readability by adding comments explaining the
    creation of WebApplication, service addition, middleware
    order importance, and HTTP request pipeline configuration.

--- WebAppEmpty/Program.cs
+++ WebAppEmpty/Program.cs
@@ -4,9 +4,19 @@ namespace WebAppEmpty
     {
         public static void Main(string[] args)
         {
+            // Create a new instance of WebApplication
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container
             var app = builder.Build();
 
+            // Add services to the container
+
+
+            // Middleware is software that's assembled into an app pipeline to handle requests and responses
+            // The order of middleware is important
+
+            // Configure the HTTP request pipeline
             if (!app.Environment.IsDevelopment())
             {
                 // HTTP Strict Transport Security (HSTS) is a web security policy mechanism that helps to protect websites
