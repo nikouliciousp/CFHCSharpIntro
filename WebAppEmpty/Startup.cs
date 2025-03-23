6635bda322612c1e61f9ba5b29f843d496dff361
perisnik <nikouliciousp@gmail.com>
2025-03-23 22:49:29 +0200

    Add Startup class for ASP.NET Core web application
    Introduces a new `Startup` class in the `WebAppEmpty` namespace.
    Includes `ConfigureServices` to add MVC services and `Configure`
    to set up the HTTP request pipeline with exception handling,
    HTTPS redirection, static file serving, and routing for controllers.

---
+++ WebAppEmpty/Startup.cs
@@ -0,0 +1,45 @@
+﻿namespace WebAppEmpty
+{
+    public class Startup
+    {
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container
+            services.AddControllersWithViews();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
